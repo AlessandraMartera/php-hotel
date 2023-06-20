2fbb65e16889ec4e45d1227443d8f53462f4b4bb
alessandra martera <ale.martera@gmail.com>
2023-06-20 17:01:13 +0200

    update

--- index.php
+++ index.php
@@ -91,15 +91,7 @@ con **una tabella**. -->
         // echo "</table>";
     ?>
 
-<table class='table'>
-    <tr>
-        <td><?php echo $hotel['name'] ?></td>  
-        <td><?php echo $hotel['name'] ?></td>  
-        <td><?php echo $hotel['name'] ?></td>  
-        <td><?php echo $hotel['name'] ?></td>  
-        <td><?php echo $hotel['name'] ?></td>  
-    </tr>
-</table>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
