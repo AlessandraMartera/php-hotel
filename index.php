8ca59aa3d1695814c925e15fdbce46f58c53cedc
alessandra martera <ale.martera@gmail.com>
2023-06-21 10:00:10 +0200

    stamp table, just one forEach

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" href="#">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
@@ -70,27 +71,76 @@ con **una tabella**. -->
             ],
     
         ];
-           
-        
-        // echo "<table class='table'>";
 
-        // // echo "<tr>";
-        // //     foreach ($hotels as $key => $value){
-        // //     };
-        // // echo "</tr>";
-
-        // foreach ($hotels as $hotel) {
-           
-        //     echo "<tr>";
-             
-        //     echo "<td>". $hotel['name'] . "</td>";
-            
-        //     echo "</tr>";
-        // };
-        
-        // echo "</table>";
+    
+   
+   
     ?>
 
+    
+ 
+
+ <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">nome</th>
+      <th scope="col">descrizione</th>
+      <th scope="col">parcheggio</th>
+      <th scope="col">voto</th>
+      <th scope="col">distanza dal centro</th>
+    </tr>
+  </thead>
+
+
+  <tbody>
+    <?php 
+    forEach ($hotels as $hotel ) {
+        echo "<tr>";
+        echo "<td>" . $hotel['name'] . "</td> ";
+        echo "<td>" . $hotel['description'] . "</td> ";
+        echo "<td>" . ($hotel['parking'] ? 'yes' : 'no') . "</td> ";
+        echo "<td>" . $hotel['vote'] . "/5 </td> ";
+        echo "<td>" . $hotel['distance_to_center'] . " Km </td> ";
+        
+        echo "</tr>";
+    }
+//     echo "
+//     <tr>
+//         <td> 
+           
+//             " .  $hotels[0]['name'] . "
+           
+//         </td>
+
+//         <td> 
+           
+//             " .  $hotels[0]['description'] . "
+           
+//         </td>
+
+//         <td>
+//            " . 
+//             if ($hotels[0]['parking'] ) {
+//                 echo "si";
+//             } else {
+//                 echo "no";
+//             }
+//            . "
+//         </td>
+
+//         <td> 
+//         " .  $hotels[0]['vote'] . "
+           
+//         </td>
+
+//         <td> 
+           
+//             " . $hotels[0]['distance_to_center'] . "
+           
+//         </td>
+//     </tr>
+// ";
+    ?>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
